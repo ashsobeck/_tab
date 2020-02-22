d136967d8e5eee11e7000e37ae753a6bb352c206
Ashton Sobeck <asobeck@g.clemson.edu>
2020-02-22 15:33:54 -0500

    took out unnecessary comments and added necessary ones

--- abot.py
+++ abot.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import os 
-import io
-import aiohttp
 import praw
 import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
+#loads the .env file that has tokens and ids for reddit needed
+#to login to discord and reddit
 load_dotenv()
+#this is for loading tokens or other things you don't want in the actual program
+# for example in a .env file, you could say TOKEN=sadfjkhorg412387234651472 (not a real token)
 TOKEN = os.getenv('DISCORD_TOKEN')
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
@@ -19,11 +21,12 @@ reddit = praw.Reddit(client_id=CLIENT_ID,
                     )
 
 
-# client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
 def get_links(submissions):
     urls = []
+    #goes through each link and sees if there is 
+    #an image link
     for sub in submissions:
         if '.png' or '.jpg' in sub.url:
             urls.append(sub.url)
@@ -33,14 +36,11 @@ def get_links(submissions):
              help='Responds with a meme from hot of r/ProgrammerHumor'
             )
 async def meme(ctx):
+    #gets the links with .jpg or .png in the name
     submissions = get_links(reddit.subreddit('ProgrammerHumor').hot(limit=50))
+    #randomly chooses the meme
     url_choice = random.choice(submissions)
-    # async with aiohttp.ClientSession() as session:
-    #     async with session.get(url_choice) as resp:
-    #         if resp.status != 200: 
-    #             return await ctx.send('Error, could not download file...')
-    #         data = io.BytesIO(await resp.read())
-    #         await ctx.send(file=bot.File(data, 'meme.png'))
+    #bot sends message
     await ctx.send(url_choice)
 
 bot.run(TOKEN)
